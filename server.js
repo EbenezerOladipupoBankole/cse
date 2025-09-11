d0ffefc7f29f464c0ac080d1675a12cf1cc05493
Ebenezerbanky <bankoleebenezer111@gmail.com>
2025-09-11 14:51:33 +0100

    .

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 // Set up Public folder / subfolder for static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Express EJS Layouts
 app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
